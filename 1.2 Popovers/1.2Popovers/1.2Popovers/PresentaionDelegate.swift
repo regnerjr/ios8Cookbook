3d1368a648b5fb76174c7e53adca7576c4cdcd97
John Regner <john@johnregner.com>
2015-07-19 08:40:45 -0500

    Added more comments about presenting popovers on iPhone, Updated readme

--- 1.2 Popovers/1.2Popovers/1.2Popovers/PresentaionDelegate.swift
+++ 1.2 Popovers/1.2Popovers/1.2Popovers/PresentaionDelegate.swift
@@ -2,6 +2,13 @@ import UIKit
 
 class PresentationDelegate: NSObject, UIAdaptivePresentationControllerDelegate {
 
+    /// Use this function to change how the popover is presented when adapting. 
+    /// Return none to show popovers when in compact Environments
+//    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+//        return UIModalPresentationStyle.None
+//
+//    }
+
     /// Present popover embeded in a Navigation Controller when we are in a compact environment
     /// In a Regular environment this is shown as a popover
     func presentationController(controller: UIPresentationController,
--- README.md
+++ README.md
@@ -8,5 +8,7 @@ to take the chance to improve my autolayout skills.
 1.1 BlurEffects - I put 2 chunks of sample code in here. One shows how to set up
 a simple BlurView, the other a Vibrancy Effect with a label.
 
-
+1.2 Popovers - This took a bit of thinking and research. (WWDC 2014 - 228)  Use
+this to set up a simple popover on iPad and also be able to modify the
+presentation for iPhone.
 
